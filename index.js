a9c476dc2f11742d87329d39d4aa841b20a0a934
Achara Chisom <acharachisom28@gmail.com>
2022-06-02 07:49:25 +0100

    add code in the js file to enable users to enter any numeric value of their choice

--- index.js
+++ index.js
@@ -5,23 +5,39 @@ let gallonVal
 let kiloVal
 let poundVal
 
-let count = 20
-let converterEl = document.getElementById("converter-el")
+let count
+let numberEl = document.getElementById("number-el")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
 
-meterVal = (count / 3.281).toFixed(3)
-feetVal = (count * 3.281).toFixed(3)
-literVal = (count / 3.785).toFixed(3)
-gallonVal = (count * 3.785).toFixed(3)
-kiloVal = (count * 2.205).toFixed(3)
-poundVal = (count / 2.205).toFixed(3)
+       
+function display(count) {
+    meterVal = (count / 3.281).toFixed(3)
+    feetVal = (count * 3.281).toFixed(3)
+    literVal = (count / 3.785).toFixed(3)
+    gallonVal = (count * 3.785).toFixed(3)
+    kiloVal = (count * 2.205).toFixed(3)
+    poundVal = (count / 2.205).toFixed(3)
 
-converterEl.textContent = count
-lengthEl.textContent = `
-    ${count} meters = ${feetVal} feet | ${count} feet = ${meterVal} meters`
-volumeEl.textContent = `
-    ${count} liters = ${literVal} gallons | ${count} gallons = ${gallonVal} liters` 
-massEl.textContent = `
-    ${count} kilos = ${kiloVal} pounds | ${count} pounds = ${poundVal} pounds`       
+    lengthEl.textContent = `
+        ${count} meters = ${feetVal} feet | ${count} feet = ${meterVal} meters`
+    volumeEl.textContent = `
+        ${count} liters = ${literVal} gallons | ${count} gallons = ${gallonVal} liters` 
+    massEl.textContent = `
+        ${count} kilos = ${kiloVal} pounds | ${count} pounds = ${poundVal} pounds`
+}
+
+reset()
+
+function reset() {
+    count = 0
+    display(count)
+    numberEl.value = ""
+}
+
+function convert() {
+    count = numberEl.value
+    display(count)
+}    
+
